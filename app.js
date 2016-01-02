12a9599174444dd273e937704d1c22a70eb76678
drewbot <drewbotka@gmail.com>
2016-01-02 11:51:05 -0500

    removed unnecessary require and abstracted the options - subject relationship

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var getProfileSubj = require('./get-profile-subj');
 var getProfile = require('./get-profile');
 var printUser = require('./print-user');
 var printUserSubj = require('./print-user-subj');
@@ -6,7 +5,17 @@ var printUserSubj = require('./print-user-subj');
 var users = process.argv.slice(2);
 
 var username = users[0];
-var subject = users[1];
+var options = users[1];
+
+////////////////////////////////////////////////////////////////////////////////
+// TODO: make a condition here to accept either one or two arguements
+// based on number passed when node app is called
+// so that getProfile only has to be called once and will run according to args passed
+// maybe the second argument passed to node accepts an object
+// based on the property(ies) defined it will call the corresponding callback function to getProfile
+////////////////////////////////////////////////////////////////////////////////
+
+var subject = options;
 
 // get the profile information desired based on callback
 getProfile(username, printUser);
